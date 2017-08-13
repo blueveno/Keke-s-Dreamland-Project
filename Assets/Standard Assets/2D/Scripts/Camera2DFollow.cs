55274b8061e4a0f1e84e145816dac3a2a12b178b
Bertaux <h4ck3r59@hotmail.fr>
2017-08-13 23:55:25 +0200

    Scene prototype updated. Add camera follow and level boundaries. Camera is blocked by the boundaries.

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -15,6 +15,17 @@ namespace UnityStandardAssets._2D
         private Vector3 m_LastTargetPosition;
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
+        
+        // TODO : put limit on level manager.
+        // Add by Bib' 13/08/17 - Level boundaries
+        [Header("Level bounds :")]
+        public bool displayBoundaries = true;
+        public Bounds bounds;
+
+        private float cameraMinX;
+        private float cameraMaxX;
+        private float cameraMinY;
+        private float cameraMaxY;
 
         // Use this for initialization
         private void Start()
@@ -22,6 +33,16 @@ namespace UnityStandardAssets._2D
             m_LastTargetPosition = target.position;
             m_OffsetZ = (transform.position - target.position).z;
             transform.parent = null;
+
+            // Add by Bib'
+            float cameraSizeY = Camera.main.orthographicSize;
+            float cameraSizeX = cameraSizeY * Screen.width / Screen.height;
+
+            cameraMinX = bounds.min.x + cameraSizeX;
+            cameraMaxX = bounds.max.x - cameraSizeX;
+
+            cameraMinY = bounds.min.y + cameraSizeY;
+            cameraMaxY = bounds.max.y - cameraSizeY;
         }
 
 
@@ -45,9 +66,27 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+            newPos.x = Mathf.Clamp(newPos.x, cameraMinX, cameraMaxX);
+            newPos.y = Mathf.Clamp(newPos.y, cameraMinY, cameraMaxY);
+
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
+        }
+        
+        private void OnDrawGizmos()
+        {
+            DisplayLevelBoundaries();
+        }
+
+        // Display boundaries of the level on the editor only.
+        private void DisplayLevelBoundaries()
+        {
+            if(displayBoundaries)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireCube(bounds.center, bounds.extents*2);
+            }
         }
     }
 }
